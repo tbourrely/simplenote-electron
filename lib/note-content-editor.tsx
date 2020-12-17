80510a7dcdd525cd17f83f639d23794292b7c570
Kat Hagan <kat@codebykat.com>
2020-12-16 20:49:52 -0800

    only focus editor if search query is empty

--- lib/note-content-editor.tsx
+++ lib/note-content-editor.tsx
@@ -170,7 +170,9 @@ class NoteContentEditor extends Component<Props> {
         });
       }
     }, SPEED_DELAY);
-    this.focusEditor();
+    if (this.state.searchQuery === '') {
+      this.focusEditor();
+    }
     this.props.storeFocusEditor(this.focusEditor);
     this.props.storeHasFocus(this.hasFocus);
     this.toggleShortcuts(true);
